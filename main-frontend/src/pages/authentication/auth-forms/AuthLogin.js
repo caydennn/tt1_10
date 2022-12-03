d644cf837b6baa55240fea6837703cfff3ecca8e
Joel19997 <joel.seah.2019@sis.smu.edu.sg>
2022-12-03 12:00:36 +0800

    Removed checkbox
    Removed keep sign in checkbox and option

--- main-frontend/src/pages/authentication/auth-forms/AuthLogin.js
+++ main-frontend/src/pages/authentication/auth-forms/AuthLogin.js
@@ -127,18 +127,6 @@ const AuthLogin = () => {
 
                             <Grid item xs={12} sx={{ mt: -1 }}>
                                 <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
-                                    <FormControlLabel
-                                        control={
-                                            <Checkbox
-                                                checked={checked}
-                                                onChange={(event) => setChecked(event.target.checked)}
-                                                name="checked"
-                                                color="primary"
-                                                size="small"
-                                            />
-                                        }
-                                        label={<Typography variant="h6">Keep me sign in</Typography>}
-                                    />
                                     <Link variant="h6" component={RouterLink} to="" color="text.primary">
                                         Forgot Password?
                                     </Link>
