b2fdc65d059855cdd7f510e36ad432dd7b676079
Joel19997 <joel.seah.2019@sis.smu.edu.sg>
2022-12-03 12:17:04 +0800

    Cleaning of pages
    Removing unneeded fields and graphs

--- main-frontend/src/menu-items/support.js
+++ main-frontend/src/menu-items/support.js
@@ -7,30 +7,4 @@ const icons = {
     QuestionOutlined
 };
 
-// ==============================|| MENU ITEMS - SAMPLE PAGE & DOCUMENTATION ||============================== //
-
-const support = {
-    id: 'support',
-    title: 'Support',
-    type: 'group',
-    children: [
-        {
-            id: 'sample-page',
-            title: 'Sample Page',
-            type: 'item',
-            url: '/sample-page',
-            icon: icons.ChromeOutlined
-        },
-        {
-            id: 'documentation',
-            title: 'Documentation',
-            type: 'item',
-            url: 'https://codedthemes.gitbook.io/mantis-react/',
-            icon: icons.QuestionOutlined,
-            external: true,
-            target: true
-        }
-    ]
-};
-
 export default support;
