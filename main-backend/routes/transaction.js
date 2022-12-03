d9c978a22e668c8ff3cdf602f4f84ff9b438eb65
Cayden <gweeyongta@gmail.com>
2022-12-03 13:28:52 +0800

    Fixed import error transactoin cud

--- main-backend/routes/transaction.js
+++ main-backend/routes/transaction.js
@@ -5,7 +5,7 @@ import {
   createTransaction,
   updateTransaction,
   deleteTransaction,
-} from "../controllers/transaction_cud.js";
+} from "../controllers/transaction.js";
 const router = Router();
 
 // R
