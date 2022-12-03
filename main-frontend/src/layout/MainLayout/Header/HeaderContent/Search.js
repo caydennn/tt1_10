e5a5c1b0272c790416ff674867d5766388e333aa
shahan alam <shahanisright@gmail.com>
2022-12-03 16:30:39 +0800

    Added display user

--- main-frontend/src/layout/MainLayout/Header/HeaderContent/Search.js
+++ main-frontend/src/layout/MainLayout/Header/HeaderContent/Search.js
@@ -1,11 +1,21 @@
 // material-ui
 import { Box, FormControl, InputAdornment, OutlinedInput } from '@mui/material';
+import { useSelector} from 'react-redux'
 
 // assets
 import { SearchOutlined } from '@ant-design/icons';
 
 // ==============================|| HEADER CONTENT - SEARCH ||============================== //
 
-const Search = () => <Box sx={{ width: '100%', ml: { xs: 0, md: 1 } }}>Hi User!</Box>;
+const Search = () => {
+    
+    const user = useSelector(state => state.user)
+
+    const displayUser = state.user ? state.user.currentUser.firstName : "User"
+
+    return (
+        <Box sx={{ width: '100%', ml: { xs: 0, md: 1 } }}>{displayUser}</Box>;
+    )
+}
 
 export default Search;
